8c010a390db5fa58d753ed3274667e5f906700cc
Md Rafiqul Islam <rafiq@Mds-iMac.local>
2019-07-23 20:41:16 -0700

    while loop, for loop

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -8,8 +8,14 @@ let interestingNumbers = [
     "Square": [1, 4, 9, 16, 25],
 ]
 var largest = 0
+
 var type: String = "None"
 
+/**
+ * Here kind -> "Prime", "Fibonacci", "Square" and
+ * numbers -> [2, 3, 5, 7, 11, 13], [1, 1, 2, 3, 5, 8], etc
+ * number -> 2, 3, 5, 1, etc.
+ */
 for (kind, numbers) in interestingNumbers {
     for number in numbers {
         if number > largest {
@@ -18,7 +24,7 @@ for (kind, numbers) in interestingNumbers {
         }
     }
 }
-print("Type: \(type), largest: \(largest)")
+// print("Type: \(type), largest: \(largest)")
 
 /**
  * Integer Type Array
@@ -28,7 +34,7 @@ print("Type: \(type), largest: \(largest)")
 var array: [Int] = []
 
 // initializing with default value
-array = Array(repeating: 0, count: 10)
+array = Array(repeating: 0, count: 5)
 
 // add item at the end of array
 array.append(100)
@@ -38,5 +44,37 @@ array[2] = 200
 
 // Iteration array
 for value in array{
-    print(value)
+    //print(value)
+}
+
+/**
+ * while loop
+ */
+
+var n = 100
+while n < 100 {
+    n *= 2
+}
+//print(n)
+
+var m = 100
+repeat {
+    m *= 2
+} while m < 100
+//print(m)
+
+/**
+ * for loop: "..<" means i < N, "..." means i <= N
+ */
+var N = 4
+for i in 0..<N {
+    print(i, terminator:" ")
+    
+    if i == N-1 {
+        print(terminator:"\n")
+    }
+}
+
+for i in 0...N {
+    print(i, terminator:" ")
 }
