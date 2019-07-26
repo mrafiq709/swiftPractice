a765e6dce164a4f5e950ac1f85e8d77aeb2e0e56
Md Rafiqul Islam <rafiq@Mds-iMac.local>
2019-07-25 19:02:36 -0700

    Generics

---
+++ Generics.playground/Contents.swift
@@ -0,0 +1,40 @@
+
+// Making an array
+func makeArray<Item>(repeating item: Item, numberOfTimes: Int) -> [Item] {
+    var result = [Item]()
+    for _ in 0..<numberOfTimes {
+        result.append(item)
+    }
+    return result
+}
+var arr = makeArray(repeating: "knock", numberOfTimes: 4)
+
+for item in arr{
+    //print(item)
+}
+
+// Reimplement the Swift standard library's optional type
+enum OptionalValue<Wrapped> {
+    case none
+    case some(Wrapped)
+}
+var possibleInteger: OptionalValue<Int> = .none
+//print(possibleInteger)
+
+possibleInteger = .some(100)
+//print(possibleInteger)
+
+
+func anyCommonElements<T: Sequence, U: Sequence>(_ lhs: T, _ rhs: U) -> Bool
+    where T.Element: Equatable, T.Element == U.Element
+{
+    for lhsItem in lhs {
+        for rhsItem in rhs {
+            if lhsItem == rhsItem {
+                return true
+            }
+        }
+    }
+    return false
+}
+anyCommonElements([1, 2, 3], [3])
